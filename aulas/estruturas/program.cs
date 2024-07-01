b0f392940d5a3d4d1ee11f899ef31121fc81abcc
Gabriel  Caetano <101360291+gabriel-caraujo@users.noreply.github.com>
2024-07-01 16:57:23 -0300

    inclusao aula matrizes

---
+++ aulas/estruturas/program.cs
@@ -0,0 +1,14 @@
+/*string [] fraudulentOrderIDs = new string [];
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+*/
+
+string [] fraudulentOrderIDs = {"A123", "B456", "C789"};
+
+Console.WriteLine ($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine ($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine ($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassingn First:{fraudulentOrderIDs[0]}");
